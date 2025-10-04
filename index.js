298cc5b440a9803a0c1709e24616620ba0c81b8d
Jimmy Laroche <jimmy.lar91@gmail.com>
2025-10-03 19:00:34 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let homeScore = 0;
+let guestScore = 0;
+
+let homeScoreEl = document.getElementById("home-score")
+let guestScoreEl = document.getElementById("guest-score")
+
+function homePlus1() {
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+}
+
+function homePlus2() {
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+}
+
+function homePlus3() {
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+}
+
+function guestPlus1() {
+    guestScore += 1
+    guestScoreEl.textContent = guestScore
+}
+
+function guestPlus2() {
+    guestScore += 2
+    guestScoreEl.textContent = guestScore
+}
+
+function guestPlus3() {
+    guestScore += 3
+    guestScoreEl.textContent = guestScore
+}
+
+function newGame() {
+    homeScoreEl.textContent = 0
+    guestScoreEl.textContent = 0
+}
+
